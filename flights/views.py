a641f6f77911c5e31585ef0159452575c927b806
Srijan-SS02 <codeofficialsrijansriv@gmail.com>
2022-12-20 07:20:43 +0530

    users app left

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight, Passengers
+from .models import Flight, Passenger
  
 # Create your views here.
 
@@ -15,8 +17,13 @@ def flight(request, flight_id):                  #Pk:Primary_key
     flight= Flight.objects.get(pk = flight_id)
     return render(request,"flights/flight.html", {
         "flight": flight,
-        "passengers":flight.passengers.all()
+        "passengers":flight.passenger.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 
-
+def book(request, flight_id):
+    flight=Flight.objects.get(pk=flight_id)
+    passenger=Passenger.objects.get(pk= int(request.POST["passenger"]))
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
